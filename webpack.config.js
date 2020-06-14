b6e07acf52555e3276248e1d568c546973a0a8db
Grant Heath <heathgr@gmail.com>
2020-06-14 16:27:29 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { DefinePlugin } = require('webpack')
+const { join } = require('path')
+
+module.exports = {
+  entry: join(__dirname, 'src/app.ts'),
+  devServer: {
+    contentBase: join(__dirname, 'static'),
+  },
+  devtool: 'eval-source-map',
+  output: {
+    filename: 'app.js',
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
